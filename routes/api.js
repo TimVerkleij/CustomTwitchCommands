d6bb2fbc2917b39042da71374736bd3cfd511606
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2020-08-26 01:10:10 +0200

    Working api

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,49 @@ var NoSQL = require('nosql');
 var db = NoSQL.load('./local.db.nosql');
 
 
-router.get('/api/', (req, res) => {
-    const obj = {
-        foo: 'bar'
-    };
-    res.json(obj);
+router.get('/customcommands/queue/', (req, res) => {
+    db.find().make(function(filter) {
+        filter.callback(function(err, response) {
+            var answer = "1v1 queue: "
+            response.forEach(myFunction);
+            function myFunction(value) {
+                answer = answer + value.user + " - "
+            }
+            res.send(answer)
+        });
+    });
 });
+
+router.get('/customcommands/adduser/:data', (req, res) => {
+    var user = req.params.data;
+    db.insert({
+        user
+    })
+    res.send(user + " has been added to the list")
+});
+
+router.get('/customcommands/deluser/:data', (req, res) => {
+    var user = req.params.data;
+    db.remove()
+        .where('user', '=', user)
+        .callback(function(error, found) {
+            if (found) {
+                 res.send(user + " has been removed from the list") 
+                } else {
+                    res.send("That user was not found")
+                }
+        });
+});
+
+
+router.get('/customcommands/deluser/', (req, res) => {
+    res.send("Please enter a user to remove from the queue")
+});
+
+router.get('/customcommands/adduser/', (req, res) => {
+    res.send("Please enter a user to add to the queue")
+});
+
 
 
 module.exports = router;
