2c60bc85ac95ecb16cbf81b9c144a941b47732fa
Tim Verkleij <tim.verkleij@student.hu.nl>
2022-07-14 16:22:57 +0200

    Add queue disable-enable function

--- routes/queue.js
+++ routes/queue.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const NoSQL = require('nosql');
 const db = NoSQL.load('./local.queue.nosql');
+const queueIsEnabled = NoSQL.load('./local.enable-queue.nosql');
 
 router.get('/v2/list/', (req, res) => {
     db.find().make(function (filter) {
@@ -39,11 +40,21 @@ router.get('/v2/queue/', (req, res) => {
 })
 
 router.get('/v2/queue/join/:user', (req, res) => {
-    var user = req.params.user.toLowerCase();
-    db.insert({
-        user
+    queueIsEnabled.find().make(function (filter) {
+        filter.callback(function (err, response) {
+            if (response.length == 0 || response[0].enabled == false) {
+                res.send("The queue is disabled. Use !enable-queue to enable it.")
+            } else {
+                var user = req.params.user.toLowerCase();
+                db.insert({
+                    user
+                })
+                res.send(user + " has joined the queue")
+            }
+        })
     })
-    res.send(user + " has joined the queue")
+
+
 })
 
 router.get('/v2/queue/leave/:user', (req, res) => {
@@ -80,6 +91,26 @@ router.get('/v2/queue/next/', (req, res) => {
                 .callback(function () {
                     res.send(`The following users were removed from the queue: ${users.join(', ')}`)
                 })
+        })
+    })
+})
+
+router.get('/v2/queue/enable/', (req, res) => {
+    queueIsEnabled.find().make(function (filter) {
+        filter.callback(function (err, response) {
+            console.log(response)
+            if (response.length == 0) {
+                queueIsEnabled.insert({
+                    enabled: true
+                })
+                res.send("Queue is now enabled")
+            } else {
+                const isEnabled = response[0].enabled
+                queueIsEnabled.update({
+                    enabled: !isEnabled
+                })
+                res.send(`Queue is now ${!isEnabled ? 'enabled':'disabled'}`)
+            }
         })
     })
 })
